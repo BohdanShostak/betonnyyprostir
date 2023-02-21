a4112fcb3aa92dc7342fc987acf115765d901d88
Bohdan Shostak <bohdanshostak93@gmail.com>
2023-02-21 22:15:49 +0200

    Add menu to all pages

---
+++ js/menu.js
@@ -0,0 +1,45 @@
+document.write('<nav class="navbar navbar-light navbar-expand-md bg-faded justify-content-center">\n' +
+    '    <div class="container-fluid">\n' +
+    '        <a href="/betonnyyprostir/index.html" class="navbar-brand d-flex w-50 me-auto">\n' +
+    '            <img src="images/logo_vertical.png" alt="Бетонний простір" height="60">\n' +
+    '        </a>\n' +
+    '        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsingNavbar3">\n' +
+    '            <span class="navbar-toggler-icon"></span>\n' +
+    '        </button>\n' +
+    '        <div class="navbar-collapse collapse w-100" id="collapsingNavbar3">\n' +
+    '            <ul class="navbar-nav w-100 justify-content-center h6">\n' +
+    '                <li class="nav-item dropdown">\n' +
+    '                    <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"> Продукція </a>\n' +
+    '                    <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarScrollingDropdown">\n' +
+    '                        <li><a class="dropdown-item" href="/betonnyyprostir/pavement.html"> Тротуарна плитка </a></li>\n' +
+    '                        <li><a class="dropdown-item" href="/betonnyyprostir/fences.html"> Бетонні паркани </a></li>\n' +
+    '                        <li><a class="dropdown-item" href="/betonnyyprostir/blocks.html"> Шлакоблок </a></li>\n' +
+    '                    </ul>\n' +
+    '                </li>\n' +
+    '                <li class="nav-item">\n' +
+    '                    <a class="nav-link" href="/betonnyyprostir/services.html">Послуги</a>\n' +
+    '                </li>\n' +
+    '                <li class="nav-item">\n' +
+    '                    <a class="nav-link" href="/betonnyyprostir/gallery.html">Галерея</a>\n' +
+    '                </li>\n' +
+    '                <li class="nav-item">\n' +
+    '                    <a class="nav-link no-text-transfer" href="/betonnyyprostir/about.html">Про нас</a>\n' +
+    '                </li>\n' +
+    '                <li class="nav-item active">\n' +
+    '                    <a class="nav-link" href="/betonnyyprostir/contacts.html"> Контакти </a>\n' +
+    '                </li>\n' +
+    '                <li class="nav-item">\n' +
+    '                    <a class="nav-link" href="https://docs.google.com/spreadsheets/d/e/2PACX-1vSowGBFVitYTUz9sc-wcSKZLTsohBVd3QBwmxF-A6Gu0tc_hQl3xKYBR2Zn7gG5tT1YJxcZJOYhaUx6/pub?gid=0&single=true&output=pdf">Каталог</a>\n' +
+    '                </li>\n' +
+    '            </ul>\n' +
+    '            <ul class="nav navbar-nav ms-auto w-100 justify-content-end">\n' +
+    '                <li class="nav-item">\n' +
+    '                    <p class="no-text-transfer"> 068 910 3710 &#160 <br> 050 760 8545 &#160 </p>\n' +
+    '                </li>\n' +
+    '                <li class="nav-item">\n' +
+    '                    <a class="nav-link" href="/files/Price.pdf" ><p class="h5"><kbd> &#8681 Прайс</kbd></p> </a>\n' +
+    '                </li>\n' +
+    '            </ul>\n' +
+    '        </div>\n' +
+    '    </div>\n' +
+    '</nav>\n');
